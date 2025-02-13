3ceb8cc1c261b1d7ad0ac2a7d7eb8b23955a7b27
bour2012 <jarawee2310@gmail.com>
2025-02-14 02:01:50 +0700

    Revert "Reapply "Merge branch 'main' into Environment""
    This reverts commit 7700de11f56b0bf286d6710c7319ae74990754fc.

--- Assets/Scripts/ResultScene/ShopManager.cs
+++ Assets/Scripts/ResultScene/ShopManager.cs
@@ -28,6 +28,15 @@ public class ShopManager : MonoBehaviour
             InventorySlot slot = mainInventory.IsEmptySlotLeft(item);
             slot.SetThisSlot(item, 1);
         }
-
+        //foreach (InventorySlot slot in mainInventory.toolinventorySlots)
+        //{
+        //    // if (slot.()) // ��Ǩ�ͺ��� Slot �����ҧ�������
+        //    //{
+        //    slot.SetThisSlot(item, 1); // ��������ŧ� Slot (�ӹǹ 1)
+        //    Debug.Log($"Added {item.name} to tool slot.");
+        //    return; // �͡�ҡ�ѧ��ѹ�ѹ����ѧ�������������
+        //            // }
+        //}
+        //}
     }
 }
