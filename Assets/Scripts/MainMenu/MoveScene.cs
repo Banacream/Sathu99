d6f0cf61962df80a36ffe995e4f874b1f1eb1521
bour2012 <jarawee2310@gmail.com>
2025-03-09 16:17:08 +0700

    Add mockup cutscenes start game

--- Assets/Scripts/MainMenu/MoveScene.cs
+++ Assets/Scripts/MainMenu/MoveScene.cs
@@ -5,13 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MoveScene : MonoBehaviour
 {
+
+    public float changTime;
+    public string sceneNameTime;
+
+
+
+    private void Update()
+    {
+        if (changTime != 0)
+        {
+            changTime -= Time.deltaTime;
+            if (changTime <= 0 && sceneNameTime != null)
+            {
+                SceneManager.LoadScene(sceneNameTime);
+            }
+            else
+                return;
+        }
+    }
+
     public static void LoadSceneByName(string sceneName)
     {
         // ตรวจสอบว่า Scene ที่เราจะโหลดมีอยู่หรือไม่
         if (Application.CanStreamedLevelBeLoaded(sceneName))
         {
        
-
             SceneManager.LoadScene(sceneName);  // โหลดฉากที่มีชื่อที่ระบุ
             Time.timeScale = 1;
         }
